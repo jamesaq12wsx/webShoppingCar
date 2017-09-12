52c1b6212687ceff7c64f07cf511dded37cd9ace
SJLIN-ZENBOOK\User <jamesaq12wsx@gmail.com>
2017-09-13 01:01:36 +0800

    WebShoppingCar index view layout

---
+++ WebShoppingCar/WebShoppingCar/Controllers/ShoppingCarController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebShoppingCar.Models;
+
+namespace WebShoppingCar.Controllers
+{
+    public class ShoppingCarController : Controller
+    {
+        // GET: ShoppingCar
+        public ActionResult Index()
+        {
+            List<ProductViewModel> products = new List<ProductViewModel>();
+
+            products.Add(new ProductViewModel
+            {
+                ProductName = "商品A",
+                ProductGroup = "商品甲區",
+                ProductPrice = 500,
+                ProductAmt = 0,
+                ProductBuy = false
+            });
+            products.Add(new ProductViewModel
+            {
+                ProductName = "商品B",
+                ProductGroup = "商品甲區",
+                ProductPrice = 500,
+                ProductAmt = 0,
+                ProductBuy = false
+            });
+            products.Add(new ProductViewModel
+            {
+                ProductName = "商品C",
+                ProductGroup = "商品甲區",
+                ProductPrice = 500,
+                ProductAmt = 0,
+                ProductBuy = false
+            });
+            products.Add(new ProductViewModel
+            {
+                ProductName = "商品D",
+                ProductGroup = "商品甲區",
+                ProductPrice = 500,
+                ProductAmt = 0,
+                ProductBuy = false
+            });
+            products.Add(new ProductViewModel
+            {
+                ProductName = "商品E",
+                ProductGroup = "商品乙區",
+                ProductPrice = 500,
+                ProductAmt = 0,
+                ProductBuy = false
+            });
+            products.Add(new ProductViewModel
+            {
+                ProductName = "商品F",
+                ProductGroup = "商品乙區",
+                ProductPrice = 500,
+                ProductAmt = 0,
+                ProductBuy = false
+            });
+            products.Add(new ProductViewModel
+            {
+                ProductName = "商品G",
+                ProductGroup = "商品乙區",
+                ProductPrice = 500,
+                ProductAmt = 0,
+                ProductBuy = false
+            });
+            products.Add(new ProductViewModel
+            {
+                ProductName = "商品H",
+                ProductGroup = "商品乙區",
+                ProductPrice = 500,
+                ProductAmt = 0,
+                ProductBuy = false
+            });
+            products.Add(new ProductViewModel
+            {
+                ProductName = "商品I",
+                ProductGroup = "商品丙區",
+                ProductPrice = 500,
+                ProductAmt = 0,
+                ProductBuy = false
+            });
+            products.Add(new ProductViewModel
+            {
+                ProductName = "商品J",
+                ProductGroup = "商品丙區",
+                ProductPrice = 500,
+                ProductAmt = 0,
+                ProductBuy = false
+            });
+            products.Add(new ProductViewModel
+            {
+                ProductName = "商品K",
+                ProductGroup = "商品丙區",
+                ProductPrice = 500,
+                ProductAmt = 0,
+                ProductBuy = false
+            });
+            products.Add(new ProductViewModel
+            {
+                ProductName = "商品L",
+                ProductGroup = "商品丙區",
+                ProductPrice = 500,
+                ProductAmt = 0,
+                ProductBuy = false
+            });
+
+
+            return View(products);
+        }
+    }
+}
---
+++ WebShoppingCar/WebShoppingCar/Models/ProductViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace WebShoppingCar.Models
+{
+    public class ProductViewModel
+    {
+        [DisplayName("商品")]
+        public string ProductName { get; set; }
+
+        [DisplayName("價錢")]
+        public int ProductPrice { get; set; }
+
+        [DisplayName("數量")]
+        public int ProductAmt { get; set; }
+
+        [DisplayName("商品區")]
+        public string ProductGroup { get; set; }
+
+        [DisplayName("是否購買")]
+        public bool ProductBuy { get; set; }
+    }
+}
